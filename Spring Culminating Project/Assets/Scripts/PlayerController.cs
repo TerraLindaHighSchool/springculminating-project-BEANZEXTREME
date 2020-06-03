001a5b75ac672eed8a7c693e61cc74a5b527ebd1
BEANZEXTREME <59098911+BEANZEXTREME@users.noreply.github.com>
2020-06-03 10:31:57 -0700

    ~1.3
    i forgot to save and i lost some stuff ofof

--- Spring Culminating Project/Assets/Scripts/PlayerController.cs
+++ Spring Culminating Project/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
 
+    //time stuff
+    private float secondsCount;
+    private int minuteCount;
+    private int hourCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +32,36 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // We turn the vehicle
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //off the interweb
+        UpdateTimerUI();
+
+        //neyeyeroeorer
+        InitiateZoomies();
+    }
+   
+    //call this on update off interweb
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        // timerText.text = hourCount + "h:" + minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+        else if (minuteCount >= 60)
+        {
+            hourCount++;
+            minuteCount = 0;
+        }
+    }
+
+    //"The Speed!"
+    public void InitiateZoomies()
+    {
+        //in testing find some better maths later
+        speed += secondsCount;
     }
 }
--- Spring Culminating Project/Assets/Scripts/SpawnManager.cs
+++ Spring Culminating Project/Assets/Scripts/SpawnManager.cs
@@ -21,14 +21,16 @@ public class SpawnManager : MonoBehaviour
     void SpawnObjects()
     {
         // Set random spawn location and random object index
-        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        Vector3 spawnLocation = new Vector3(Random.Range(-42, 85), 5, 65);
         int index = Random.Range(0, objectPrefabs.Length);
         /*
         // If game is still active, spawn new object
         if (!playerControllerScript.gameOver)
         {
+        */
             Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
-        }
+        /*
+         }
         */
     }
 }
